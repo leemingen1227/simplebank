5ed33e4ae445f026c05d5ba4343ef1a29a167b24
johnson <leemingen1227@gmail.com>
2023-02-10 21:00:19 +0800

    add unit tests for authorization middleware

--- db/query/account.sql
+++ db/query/account.sql
@@ -24,9 +24,10 @@ RETURNING *;
 
 -- name: ListAccounts :many
 SELECT * FROM accounts
+WHERE owner = $1
 ORDER BY id
-LIMIT $1
-OFFSET $2;
+LIMIT $2
+OFFSET $3;
 
 -- name: UpdateAccounts :one
 UPDATE accounts
